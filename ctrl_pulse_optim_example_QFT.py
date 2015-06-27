070b585fcc005027ae1eff6327511fc9cfe24d52
Alexander Pitchford <alex.pitchford@gmail.com>
2015-06-27 17:55:05 +0100

    More examples added

---
+++ ctrl_pulse_optim_example_QFT.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 28 17:18:29 2014
+
+@author: Alexander Pitchford
+@email1: agp1@aber.ac.uk
+@email2: alex.pitchford@gmail.com
+@organization: Aberystwyth University
+@supervisor: Daniel Burgarth
+
+The code in this file was is intended for use in not-for-profit research,
+teaching, and learning. Any other applications may require additional
+licensing
+
+Example to demonstrate using the control library to determine control
+pulses using the ctrlpulseoptim.create_pulse_optimizer function to 
+generate an Optimizer object, through which the configuration can be
+manipulated before running the optmisation algorithm. In this case it is
+demonstrated by modifying the initial ctrl pulses.
+
+The (default) L-BFGS-B algorithm is used to optimise the pulse to
+minimise the fidelity error, which is equivalent maximising the fidelity
+to optimal value of 1.
+
+The system in this example is two qubits in constant fields in x, y and z
+with a variable independant controls fields in x and y acting on each qubit
+The target evolution is the QFT gate. The user can experiment with the
+different:
+    phase options - phase_option = SU or PSU
+    propagtor computer type prop_type = DIAG or FRECHET
+    fidelity measures - fid_type = UNIT or TRACEDIFF
+
+The user can experiment with the timeslicing, by means of changing the
+number of timeslots and/or total time for the evolution.
+Different initial (starting) pulse types can be tried.
+The initial and final pulses are displayed in a plot
+
+Note the physics of this example was taken from a demo in:
+DYNAMO - Dynamic Framework for Quantum Optimal Control
+See Machnes et.al., arXiv.1011.4874
+"""
+import numpy as np
+import numpy.matlib as mat
+from numpy.matlib import kron
+import matplotlib.pyplot as plt
+import datetime
+
+#QuTiP
+from qutip import Qobj, identity
+import qutip.logging as logging
+logger = logging.get_logger()
+#QuTiP control modules
+import qutip.control.pulseoptim as cpo
+import qutip.control.pulsegen as pulsegen
+from qutip.qip.algorithms import qft
+
+example_name = 'QFT'
+log_level=logging.INFO
+# ****************************************************************
+# Define the physics of the problem
+nSpins = 2
+Sx = np.array([[0, 1], [1, 0]], dtype=complex)
+Sy = np.array([[0, -1j], [1j, 0]], dtype=complex)
+Sz = np.array([[1, 0], [0, -1]], dtype=complex)
+Si = mat.eye(2)/2
+
+H_d = Qobj(0.5*(kron(Sx, Sx) + kron(Sy, Sy) + kron(Sz, Sz)))
+H_c = [Qobj(kron(Sx, Si)), Qobj(kron(Sy, Si)), 
+           Qobj(kron(Si, Sx)), Qobj(kron(Si, Sy))]
+n_ctrls = len(H_c)
+
+U_0 = identity(2**nSpins)
+
+# Quantum Fourier Transform gate
+# This produces values with small rounding errors
+U_targ = qft.qft(nSpins)
+# can use this for nSpins=2
+#U_targ = np.array([
+#    [0.5+0.0j,  0.5+0.0j,  0.5+0.0j,  0.5+0.0j],
+#    [0.5+0.0j,  0.0+0.5j, -0.5+0.0j, -0.0-0.5j],
+#    [0.5+0.0j, -0.5+0.0j,  0.5+0.0j, -0.5+0.0j],
+#    [0.5+0.0j,  0.0-0.5j, -0.5+0.0j,  0.0+0.5j],
+#    ])
+
+# ***** Define time evolution parameters *****
+# Number of time slots
+n_ts = 100
+# Time allowed for the evolution
+evo_time = 10
+
+# ***** Define the termination conditions *****
+# Fidelity error target
+fid_err_targ = 1e-5
+# Maximum iterations for the optisation algorithm
+max_iter = 200
+# Maximum (elapsed) time allowed in seconds
+max_wall_time = 120
+# Minimum gradient (sum of gradients squared)
+# as this tends to 0 -> local minima has been found
+min_grad = 1e-20
+
+# Initial pulse type
+# pulse type alternatives: RND|ZERO|LIN|SINE|SQUARE|SAW|TRIANGLE|
+p_type = 'RNDWAVES'
+# *************************************************************
+# File extension for output files
+
+f_ext = "{}_n_ts{}_ptype{}.txt".format(example_name, n_ts, p_type)
+
+print ""
+print "***********************************"
+print "Creating optimiser objects"
+optim = cpo.create_pulse_optimizer(H_d, H_c, U_0, U_targ, n_ts, evo_time, 
+                amp_lbound=-10.0, amp_ubound=10.0, 
+                fid_err_targ=fid_err_targ, min_grad=min_grad, 
+                max_iter=max_iter, max_wall_time=max_wall_time, 
+#                optim_method='LBFGSB', 
+#                method_params={'max_metric_corr':20, 'accuracy_factor':1e8},
+#                optim_method='fmin_l_bfgs_b',
+                optim_method='l-bfgs-b',
+                dyn_type='UNIT', 
+                prop_type='DIAG', 
+                fid_type='UNIT', fid_params={'phase_option':'SU'}, 
+                init_pulse_type=p_type, pulse_scaling=1.0,
+                log_level=log_level, gen_stats=True)
+                
+print ""
+print "***********************************"
+print "Configuring optimiser objects"
+# **** Set some optimiser config parameters ****
+optim.test_out_files = 0
+dyn = optim.dynamics
+
+# check method params
+#print("max_metric_corr: {}".format(optim.max_metric_corr))
+#print("accuracy_factor: {}".format(optim.accuracy_factor))
+#print("phase_option: {}".format(dyn.fid_computer.phase_option))
+
+
+dyn.test_out_files = 0
+# Generate different pulses for each control
+p_gen = optim.pulse_generator
+init_amps = np.zeros([n_ts, n_ctrls])
+if (p_gen.periodic):
+    phase_diff = np.pi / n_ctrls
+    for j in range(n_ctrls):
+        init_amps[:, j] = p_gen.gen_pulse(start_phase=phase_diff*j)
+elif (isinstance(p_gen, pulsegen.PulseGenLinear)):
+    for j in range(n_ctrls):
+        p_gen.scaling = float(j) - float(n_ctrls - 1)/2
+        init_amps[:, j] = p_gen.gen_pulse()
+elif (isinstance(p_gen, pulsegen.PulseGenZero)):
+    for j in range(n_ctrls):
+        p_gen.offset = sf = float(j) - float(n_ctrls - 1)/2
+        init_amps[:, j] = p_gen.gen_pulse()
+else:
+    # Should be random pulse
+    for j in range(n_ctrls):
+        init_amps[:, j] = p_gen.gen_pulse()
+
+dyn.initialize_controls(init_amps)
+
+# Save initial amplitudes to a text file
+pulsefile = "ctrl_amps_initial_" + f_ext
+dyn.save_amps(pulsefile)
+if (log_level <= logging.INFO):
+    print "Initial amplitudes output to file: " + pulsefile
+
+print "***********************************"
+print "Starting pulse optimisation"
+result = optim.run_optimization()
+
+# Save final amplitudes to a text file
+pulsefile = "ctrl_amps_final_" + f_ext
+dyn.save_amps(pulsefile)
+if (log_level <= logging.INFO):
+    print "Final amplitudes output to file: " + pulsefile
+        
+print ""
+print "***********************************"
+print "Optimising complete. Stats follow:"
+result.stats.report()
+print ""
+print "Final evolution"
+print result.evo_full_final
+print ""
+
+print "********* Summary *****************"
+print "Final fidelity error {}".format(result.fid_err)
+print "Terminated due to {}".format(result.termination_reason)
+print "Number of iterations {}".format(result.num_iter)
+#print "wall time: ", result.wall_time
+print "Completed in {} HH:MM:SS.US".\
+        format(datetime.timedelta(seconds=result.wall_time))
+# print "Final gradient normal {}".format(result.grad_norm_final)
+print "***********************************"
+
+# Plot the initial and final amplitudes
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(2, 1, 1)
+ax1.set_title("Initial ctrl amps")
+ax1.set_xlabel("Time")
+ax1.set_ylabel("Control amplitude")
+t = result.time[:n_ts]
+for j in range(n_ctrls):
+    amps = result.initial_amps[:, j]
+    ax1.plot(t, amps)
+ax2 = fig1.add_subplot(2, 1, 2)
+ax2.set_title("Optimised Control Sequences")
+ax2.set_xlabel("Time")
+ax2.set_ylabel("Control amplitude")
+for j in range(n_ctrls):
+    amps = result.final_amps[:, j]
+    ax2.plot(t, amps)
+
+plt.show()
+
+
--- ctrl_pulse_optim_example_Symplectic.py
+++ ctrl_pulse_optim_example_Symplectic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 10 15:12:53 2014
+Created on Tue Oct 28 15:14:58 2014
+
 @author: Alexander Pitchford
 @email1: agp1@aber.ac.uk
 @email2: alex.pitchford@gmail.com
@@ -13,86 +14,74 @@ The (default) L-BFGS-B algorithm is used to optimise the pulse to
 minimise the fidelity error, which in this case is given by the
 'Trace difference' norm.
 
-This in an open quantum system example, with a single qubit subject to
-an amplitude damping channel. The target evolution is the Hadamard gate.
-The user can experiment with the strength of the amplitude damping by
-changing the gamma variable value
+This in an Symplectic quantum system example, with two coupled oscillators
 
 The user can experiment with the timeslicing, by means of changing the
 number of timeslots and/or total time for the evolution.
 Different initial (starting) pulse types can be tried.
 The initial and final pulses are displayed in a plot
-
 """
+
 import numpy as np
 import numpy.matlib as mat
-from numpy.matlib import kron
+#import scipy.linalg as la
 import matplotlib.pyplot as plt
 import datetime
 
 #QuTiP
-from qutip import Qobj, identity, sigmax, sigmay, sigmaz, tensor
-from qutip.qip import hadamard_transform
+from qutip import Qobj, identity
 import qutip.logging as logging
 logger = logging.get_logger()
 #QuTiP control modules
 import qutip.control.pulseoptim as cpo
+import qutip.control.symplectic as sympl
 
-example_name = 'Lindblad'
+example_name = 'Symplectic'
 log_level = logging.DEBUG
 
 # ****************************************************************
 # Define the physics of the problem
-Sx = sigmax()
-Sy = sigmay()
-Sz = sigmaz()
-Si = identity(2)
-
-Sd = Qobj(np.array([[0, 1],
-             [0, 0]]))
-Sm = Qobj(np.array([[0, 0],
-             [1, 0]]))
-Sd_m = Qobj(np.array([[1, 0],
-              [0, 0]]))
-Sm_d = Qobj(np.array([[0, 0],
-              [0, 1]]))
-
-#Amplitude damping#
-#Damping rate:
-gamma = 1.0
-L0_Ad = gamma*(2*tensor(Sm, Sd.trans()) - 
-            (tensor(Sd_m, Si) + tensor(Si, Sd_m.trans())))
-#sigma X control
-LC_x = -1j*(tensor(Sx, Si) - tensor(Si, Sx))
-#sigma Y control
-LC_y = -1j*(tensor(Sy, Si) - tensor(Si, Sy.trans()))
-#sigma Z control
-LC_z = -1j*(tensor(Sz, Si) - tensor(Si, Sz))
 
 #Drift
-drift = L0_Ad
-#Controls
-ctrls = [LC_z, LC_x]
-# Number of ctrls
+w1 = 1
+w2 = 1
+g1 = 0.2
+A0 = Qobj(np.array([[w1, 0, g1, 0], 
+                   [0, w1, 0, g1], 
+                   [g1, 0, w2, 0], 
+                   [0, g1, 0, w2]]))
+
+#Control
+Ac = Qobj(np.array([[1, 0, 0, 0,], \
+                    [0, 1, 0, 0], \
+                    [0, 0, 0, 0], \
+                    [0, 0, 0, 0]]))
+ctrls = [Ac]        
 n_ctrls = len(ctrls)
 
 initial = identity(4)
-#Target
-#Hadamard gate
-had_gate = hadamard_transform(1)
-target_DP = tensor(had_gate, had_gate)
+
+# Target
+a = 1
+Ag = np.array([[0, 0, a, 0], 
+                [0, 0, 0, a], 
+                [a, 0, 0, 0], 
+                [0, a, 0, 0]])
+               
+Sg = Qobj(sympl.calc_omega(2).dot(Ag)).expm()
+
 
 # ***** Define time evolution parameters *****
 # Number of time slots
-n_ts = 200
+n_ts = 1000
 # Time allowed for the evolution
-evo_time = 2
+evo_time = 10
 
 # ***** Define the termination conditions *****
 # Fidelity error target
-fid_err_targ = 1e-3
+fid_err_targ = 1e-10
 # Maximum iterations for the optisation algorithm
-max_iter = 200
+max_iter = 500
 # Maximum (elapsed) time allowed in seconds
 max_wall_time = 30
 # Minimum gradient (sum of gradients squared)
@@ -102,7 +91,7 @@ min_grad = 1e-20
 
 # Initial pulse type
 # pulse type alternatives: RND|ZERO|LIN|SINE|SQUARE|SAW|TRIANGLE|
-p_type = 'LIN'
+p_type = 'ZERO'
 # *************************************************************
 # File extension for output files
 
@@ -112,20 +101,22 @@ f_ext = "{}_n_ts{}_ptype{}.txt".format(example_name, n_ts, p_type)
 print ""
 print "***********************************"
 print "Starting pulse optimisation"
-# Note that this call will take the defaults
-#    dyn_type='GEN_MAT'
+# Note that this call uses
+#    dyn_type='SYMPL'
 # This means that matrices that describe the dynamics are assumed to be
-# general, i.e. the propagator can be calculated using:
-# expm(combined_dynamics*dt)
+# Symplectic, i.e. the propagator can be calculated using 
+# expm(combined_dynamics.omega*dt)
+# This has defaults for:
 #    prop_type='FRECHET'
-# and the propagators and their gradients will be calculated using the
+# therefore the propagators and their gradients will be calculated using the
 # Frechet method, i.e. an exact gradent
 #    fid_type='TRACEDIFF'
-# and that the fidelity error, i.e. distance from the target, is give
+# so that the fidelity error, i.e. distance from the target, is give
 # by the trace of the difference between the target and evolved operators 
-result = cpo.optimize_pulse(drift, ctrls, initial, target_DP, n_ts, evo_time, 
+result = cpo.optimize_pulse(A0, ctrls, initial, Sg, n_ts, evo_time, 
                 fid_err_targ=fid_err_targ, min_grad=min_grad, 
                 max_iter=max_iter, max_wall_time=max_wall_time, 
+                dyn_type='SYMPL', \
                 out_file_ext=f_ext, init_pulse_type=p_type, 
                 log_level=log_level, gen_stats=True)
 
@@ -167,3 +158,4 @@ for j in range(n_ctrls):
     ax2.plot(t, amps)
 
 plt.show()
+
